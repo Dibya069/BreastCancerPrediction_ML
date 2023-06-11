85a4f78f5f53911c1a94cd5dc87d41eeaba65275
Dibya069 <dibyamohanty069@gmail.com>
2023-06-11 12:15:37 +0530

    application.py(3) and form.html(3)

--- application.py
+++ application.py
@@ -16,44 +16,44 @@ def predict_datapoint():
     
     else:
         data = CustomData(
-            mean_radius = float(request.form.get('mean radius')),
-            mean_texture = float(request.form.get('mean texture')),
-            mean_perimeter = float(request.form.get('mean perimeter')),
-            mean_area = float(request.form.get('mean area')),
-            mean_smoothness = float(request.form.get('mean smoothness')),
-            mean_compactness = float(request.form.get('mean compactness')),
-            mean_concavity = float(request.form.get('mean concavity')),
-            mean_concave_points = float(request.form.get('mean concave points')),
-            mean_symmetry = float(request.form.get('mean symmetry')),
-            mean_fractal_dimension = float(request.form.get('mean fractal dimension')),
-            radius_error = float(request.form.get('radius error')),
-            texture_error = float(request.form.get('texture error')),
-            perimeter_error = float(request.form.get('perimeter error')),
-            area_error = float(request.form.get('area error')),
-            smoothness_error = float(request.form.get('smoothness error')),
-            compactness_error = float(request.form.get('compactness error')),
-            concavity_error = float(request.form.get('concavity error')),
-            concave_points_error = float(request.form.get('concave points error')),
-            symmetry_error = float(request.form.get('symmetry error')),
-            fractal_dimension_error = float(request.form.get('fractal dimension error')),
-            worst_radius = float(request.form.get('worst radius')),
-            worst_texture = float(request.form.get('worst texture')),
-            worst_perimeter = float(request.form.get('worst perimeter')),
-            worst_area = float(request.form.get('worst area')),
-            worst_smoothness = float(request.form.get('worst smoothness')),
-            worst_compactness = float(request.form.get('worst compactness')),
-            worst_concavity = float(request.form.get('worst concavity')),
-            worst_concave_points = float(request.form.get('worst concave points')),
-            worst_symmetry = float(request.form.get('worst symmetry')),
-            worst_fractal_dimension = float(request.form.get('worst fractal dimension'))
+            mean_radius = float(request.form.get('mean_radius')),
+            mean_texture = float(request.form.get('mean_texture')),
+            mean_perimeter = float(request.form.get('mean_perimeter')),
+            mean_area = float(request.form.get('mean_area')),
+            mean_smoothness = float(request.form.get('mean_smoothness')),
+            mean_compactness = float(request.form.get('mean_compactness')),
+            mean_concavity = float(request.form.get('mean_concavity')),
+            mean_concave_points = float(request.form.get('mean_concave_points')),
+            mean_symmetry = float(request.form.get('mean_symmetry')),
+            mean_fractal_dimension = float(request.form.get('mean_fractal_dimension')),
+            radius_error = float(request.form.get('radius_error')),
+            texture_error = float(request.form.get('texture_error')),
+            perimeter_error = float(request.form.get('perimeter_error')),
+            area_error = float(request.form.get('area_error')),
+            smoothness_error = float(request.form.get('smoothness_error')),
+            compactness_error = float(request.form.get('compactness_error')),
+            concavity_error = float(request.form.get('concavity_error')),
+            concave_points_error = float(request.form.get('concave_points_error')),
+            symmetry_error = float(request.form.get('symmetry_error')),
+            fractal_dimension_error = float(request.form.get('fractal_dimension_error')),
+            worst_radius = float(request.form.get('worst_radius')),
+            worst_texture = float(request.form.get('worst_texture')),
+            worst_perimeter = float(request.form.get('worst_perimeter')),
+            worst_area = float(request.form.get('worst_area')),
+            worst_smoothness = float(request.form.get('worst_smoothness')),
+            worst_compactness = float(request.form.get('worst_compactness')),
+            worst_concavity = float(request.form.get('worst_concavity')),
+            worst_concave_points = float(request.form.get('worst_concave_points')),
+            worst_symmetry = float(request.form.get('worst_symmetry')),
+            worst_fractal_dimension = float(request.form.get('worst_fractal_dimension'))
         )
         final_new_data=data.get_data_as_dataframe()
-        predict_pipeline=predict_pipeline()
-        pred=predict_pipeline.predict(final_new_data)
+        predict = PredictPipeline()
+        pred=predict.predict(final_new_data)
 
         results=round(pred[0],2)
 
-        return render_template('predicttion.html',final_result=results)
+        return render_template('prediction.html',final_result=results)
     
 
 if __name__=="__main__":
